d59b3d3a65bae22d5cd42ee297e0aa4e56756484
serhatprogramming <serhatprogramming@gmail.com>
2023-04-15 12:38:56 -0400

    queryclient added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 const App = () => {
   const addNote = async (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    console.log(content)
-  }
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = "";
+    console.log(content);
+  };
 
   const toggleImportance = (note) => {
-    console.log('toggle importance of', note.id)
-  }
+    console.log("toggle importance of", note.id);
+  };
 
-  const notes = []
+  const notes = [];
 
-  return(
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
         <input name="note" />
         <button type="submit">add</button>
       </form>
-      {notes.map(note =>
+      {notes.map((note) => (
         <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
-          <strong> {note.important ? 'important' : ''}</strong>
+          {note.content}
+          <strong> {note.important ? "important" : ""}</strong>
         </li>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App'
+import { QueryClient, QueryClientProvider } from "react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+import App from "./App";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
